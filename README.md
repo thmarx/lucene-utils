e6f39c84d9e3c2072435620ed3efda9725fe2a20
Thorsten Marx <t.marx@eggheads.de>
2023-09-26 09:35:47 +0200

    readme added license info

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# MultiSearcherManager
+
+Utility class IndexSearcher instances across multiple threads. It uses MultiReader, so it's possible to access
+multiple lucene directories.
+
+## Usage
+
+```java
+List<IndexReader> readers = List.of(
+	DirectoryReader.open(dir1),
+	DirectoryReader.open(dir2)
+);
+MultiSearcherManager sm = new MultiSearcherManager(readers.toArray(IndexReader[]::new));
+
+// add, remove or update documents
+
+sm.maybeRefreshBlocking();	
+	
+IndexSearcher searcher = sm.acquire();
+try {
+	int count = searcher.count(new MatchAllDocsQuery());
+	System.out.println(count);
+} finally {
+	sm.release(searcher);
+}
+
+sm.close();
+```
+
+# ShardedIndex
+
+Whenever your local lucene index hits the document limit (~2 Billion documents) and you need to scale horizontally, 
+ShardedIndex may be a solution for you.
+
+```java
+ShardedIndex shardedIndex = ShardedIndexConfiguration.builder()
+	.numberOfShards(5)														// the number of shards
+	.strategy(new ShardSizeShardingStrategy())								// the sharding strategy
+	.shardsDirectory(Path.of("index-directory"))							// base directory where all shards will be stored
+	.shardConfiguration(ShardConfiguration.builder()						// configuration for the shards
+			.analyzer(new StandardAnalyzer())
+			.build()
+	).build();
+
+shardedIndex = new ShardedIndex(config);
+shardedIndex.open();
+
+// make use
+
+// use MultiSearchManager to access the shards
+IndexSearcher searcher = shardedIndex.getSearcherManager().acquire();
+try {
+	// search as usual
+} finally {
+	shardedIndex.getSearcherManager().release(searcher);
+}
+
+shardedIndex.close();
+```
--- src/test/java/com/github/thmarx/lucene/utils/index/ShardedIndexNGTest.java
+++ src/test/java/com/github/thmarx/lucene/utils/index/ShardedIndexNGTest.java
@@ -4,21 +4,36 @@
  */
 package com.github.thmarx.lucene.utils.index;
 
-import com.github.thmarx.lucene.utils.index.ShardedIndex;
-import com.github.thmarx.lucene.utils.index.ShardConfiguration;
-import com.github.thmarx.lucene.utils.index.ShardSizeShardingStrategy;
-import com.github.thmarx.lucene.utils.index.ShardedIndexConfiguration;
+/*-
+ * #%L
+ * lucene-utils
+ * %%
+ * Copyright (C) 2023 Marx-Software
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Map;
-import java.util.UUID;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
-import static org.testng.Assert.*;
+import org.apache.lucene.search.TopDocs;
 import org.testng.annotations.Test;
 
 /**
@@ -65,7 +80,48 @@ public class ShardedIndexNGTest {
 		} finally {
 			shardedIndex.close();
 		}
+	}
+	
+	@Test
+	public void test_Get_document() throws IOException {
 
+		var config = ShardedIndexConfiguration.builder()
+				.numberOfShards(5)
+				.strategy(new ShardSizeShardingStrategy())
+				.shardsDirectory(Path.of("target/index-" + System.currentTimeMillis()))
+				.shardConfiguration(ShardConfiguration.builder()
+						.analyzer(new StandardAnalyzer())
+						.build()
+				)
+				.build();
+
+		var shardedIndex = new ShardedIndex(config);
+		shardedIndex.open();
+		try {
+			for (int i = 0; i < 10; i++) {
+				Document doc = document(Map.of(
+						"id", "" + i
+				));
+				shardedIndex.addDocument(doc);
+			}
+
+			shardedIndex.getSearcherManager().maybeRefreshBlocking();
+			
+			IndexSearcher searcher = shardedIndex.getSearcherManager().acquire();
+			try {
+				
+				TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), 10);
+				
+				for (var scoreDoc : topDocs.scoreDocs) {
+					var document = searcher.getIndexReader().storedFields().document(scoreDoc.doc);
+					System.out.println("document: " + document);
+				}
+			} finally {
+				shardedIndex.getSearcherManager().release(searcher);
+			}
+		} finally {
+			shardedIndex.close();
+		}
 	}
 
 	private Document document(final Map<String, String> fields) {
